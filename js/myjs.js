254f0424062cdefb73b93e0c41d08c84ca89a34c
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-26 13:27:04 +0200

    func: insert from input toDo list item

--- js/myjs.js
+++ js/myjs.js
@@ -3,6 +3,7 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
+            newTextObj: {text: '', done: false},
             toDo: [
                 {
                     text: 'Fare la spesa',
@@ -22,6 +23,9 @@ createApp({
     methods: {
         eliminate(index){
             this.toDo.splice(index, 1);
+        },
+        aggiungiToDo(){
+            this.toDo.unshift(this.newTextObj);
         }
         
     },
