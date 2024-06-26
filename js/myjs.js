ac5a9cd8fe2aa8c97df9aa5b946835e676249f33
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-26 13:00:36 +0200

    add: list in page with vue

--- js/myjs.js
+++ js/myjs.js
@@ -3,13 +3,26 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
-
+            toDo: [
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: false
+                },
+            ]
         }
     },
     methods: {
 
     },
     mounted(){
-        
+
     }
 }).mount('#app');
